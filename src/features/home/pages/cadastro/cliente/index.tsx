27d92b36ad0c979d71a070198f84bde639c28241
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2026-02-22 22:53:53 -0300

    padronizando

--- src/features/home/pages/cadastro/cliente/index.tsx
+++ src/features/home/pages/cadastro/cliente/index.tsx
@@ -6,12 +6,15 @@ import FormModal from './form.modal';
 import { Plus } from 'lucide-react';
 import Form from '../../../../../ui/form';
 import Heading from '../../../../../ui/typography/heading';
+import { Spinner } from '../../../../../ui/spinner';
 
 const CadastroCliente: React.FC = () => {
   const [loading, setLoading] = React.useTransition();
 
   const handleSubmitSearchAsync = async (data: { pesquisar: string }) => {
-    console.log(data);
+    setLoading(() => {
+      console.log(data);
+    });
   };
 
   return (
